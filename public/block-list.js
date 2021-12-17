4c1a1418b4f4601c6a164c536b8aa6d5660547b6
brainTrain <abductee@gmail.com>
2021-12-16 23:01:46 -0800

    sloppy incomplete logic to maybe try and block all threads
    * need to maybe do sibling/child instead?

--- public/block-list.js
+++ public/block-list.js
@@ -5,13 +5,23 @@ function applyBlockList(blockObject) {
 
   $posts.forEach(($post) => {
     const $topic = $post.querySelector('a');
+    const $topicThread = $topic.querySelector('ul');
     const $user = $post.querySelector('.membername');
     const userName = $user.textContent;
     const isBlockedUser = blockObject[userName]?.isBlocked;
+    const isFullyBlockedUser = blockObject[userName]?.isFullyBlocked;
 
     if (isBlockedUser) {
       $user.textContent = ZERO_WIDTH_WHITE_SPACE;
       $topic.textContent = ZERO_WIDTH_WHITE_SPACE;
+    }
+
+    if (isFullyBlockedUser) {
+      $topic.style = 'display: none';
+
+      if ($topicThread) {
+        $topicThread.parentElement.style = 'display: none';
+      }
     }
   });
 }
@@ -20,18 +30,22 @@ const blockObject = {
   abductee: {
     name: 'abductee',
     isBlocked: true,
+    isFullyBlocked: true,
   },
   bngvosnwaj: {
     name: 'bngvosnwaj',
     isBlocked: true,
+    isFullyBlocked: true,
   },
   bngvcmtfzj: {
     name: 'bngvcmtfzj',
     isBlocked: true,
+    isFullyBlocked: true,
   },
   bngvtadfqj: {
     name: 'bngvtadfqj',
     isBlocked: true,
+    isFullyBlocked: true,
   },
 };
 
